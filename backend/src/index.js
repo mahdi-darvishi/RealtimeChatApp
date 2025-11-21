c09f0f59e6c6b183dc1db63d036431487a7fb20c
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-11-21 12:50:02 +0330

    Add message handling functionality with routes and controller methods

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import dotenv from "dotenv";
 import { conectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -11,6 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
